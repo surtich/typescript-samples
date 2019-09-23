01488b69c8558c7052bbbafcaa8c6cf5c7dbb485
surtich <surtich@gmail.com>
2019-09-23 13:44:49 +0200

    Written template of map with reduce

--- src/map.ts
+++ src/map.ts
@@ -5,3 +5,16 @@ export default function map<T, R>(f: (x: T) => R, xs: T[]): R[] {
     return [f(xs[0])].concat(map(f, xs.slice(1)));
   }
 }
+
+/*
+
+import reduce from "./reduce";
+
+var mapReducer =  function<T, R>(f: (x: T) => R) {
+  return function reducer (???) {}
+};
+export default function map<T, R>(f: (x: T) => R, xs: T[]): R[] {
+  return reduce(mapReducer(f), ?????)
+}
+
+*/
